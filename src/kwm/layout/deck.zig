690e1ecb9ad9545e43f11603ebb89309d4e02665
kewuaa <1692525710@qq.com>
2026-03-12 09:39:39 +0800

    add `master_location` for deck layout

--- src/kwm/layout/deck.zig
+++ src/kwm/layout/deck.zig
@@ -3,13 +3,17 @@ const Self = @This();
 const std = @import("std");
 const log = std.log.scoped(.deck);
 
+const types = @import("../types.zig");
 const Context = @import("../context.zig");
 const Output = @import("../output.zig");
 const Window = @import("../window.zig");
 
+pub const MasterLocation = types.LayoutMasterLocation;
+
 
 inner_gap: i32,
 outer_gap: i32,
+master_location: MasterLocation,
 
 
 pub fn arrange(self: *const Self, output: *Output) void {
@@ -44,17 +48,53 @@ pub fn arrange(self: *const Self, output: *Output) void {
             if (!found_stack_top) {
                 found_stack_top = true;
 
-                const master_w = @divFloor(w, 2);
-                const stack_w = w - master_w - self.inner_gap;
+                const master_w, const stack_w = switch (self.master_location) {
+                    .left, .right => .{ @divFloor(w, 2), w - @divFloor(w, 2) - self.inner_gap },
+                    .top, .bottom => .{ @divFloor(h, 2), h - @divFloor(h, 2) - self.inner_gap },
+                };
 
-                // correct master dimension
-                master.unbound_resize(master_w, h);
+                switch (self.master_location) {
+                    .left => {
+                        master.unbound_move(self.outer_gap, self.outer_gap);
+                        master.unbound_resize(master_w, h);
 
-                window.unbound_move(
-                    self.outer_gap + master_w + self.inner_gap,
-                    self.outer_gap,
-                );
-                window.unbound_resize(stack_w, h);
+                        window.unbound_move(
+                            self.outer_gap + master_w + self.inner_gap,
+                            self.outer_gap,
+                        );
+                        window.unbound_resize(stack_w, h);
+                    },
+                    .right => {
+                        master.unbound_move(
+                            self.outer_gap + stack_w + self.inner_gap,
+                            self.outer_gap,
+                        );
+                        master.unbound_resize(master_w, h);
+
+                        window.unbound_move(self.outer_gap, self.outer_gap);
+                        window.unbound_resize(stack_w, h);
+                    },
+                    .top => {
+                        master.unbound_move(self.outer_gap, self.outer_gap);
+                        master.unbound_resize(h, master_w);
+
+                        window.unbound_move(
+                            self.outer_gap,
+                            self.outer_gap + master_w + self.inner_gap,
+                        );
+                        window.unbound_resize(h, stack_w);
+                    },
+                    .bottom => {
+                        master.unbound_move(
+                            self.outer_gap,
+                            self.outer_gap + stack_w + self.inner_gap,
+                        );
+                        master.unbound_resize(h, master_w);
+
+                        window.unbound_move(self.outer_gap, self.outer_gap);
+                        window.unbound_resize(h, stack_w);
+                    }
+                }
             } else {
                 window.hide();
             }
